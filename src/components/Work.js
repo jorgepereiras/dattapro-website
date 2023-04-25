15cd60b71e7177a33d47ad14ad725c09c1e5438f
Jorge Pereira <jorgepereira.s@gmail.com>
2023-04-24 20:52:16 -0600

    Work is finished

--- src/components/Work.js
+++ src/components/Work.js
@@ -11,14 +11,19 @@ const Work = () => {
     <section className='section' id='work'>
       <div className='container mx-auto'>
         <div className='flex flex-col lg:flex-row gap-x-10'>
-          <div className='flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0'>
+          <motion.div 
+            variants={fadeIn('right', 0.2)}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{ once: false, amount: 0.3 }}
+            className='flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0'>
             {/* text */}
-            <div className='flex flex-col lg:flex-row gap-x-10'>
+            <div>
               <h2 className='h2 leading-tight text-emerald-400'>My Latest <br />Work</h2>
-              <p className='max-w-sm mb-16'>Dattapro are a Software Development House. We especialize in Cloud Software Sevelopment and User Experience Design. We are always ready to help you to grow your brand or product.</p>
+              <p className='max-w-sm mb-16'>Dattapro are a Software Development House. We especialize in Cloud Software Sevelopment and User Experience Design. We are always ready to help you.</p>
               <button className='btn btn-sm'>View all projects</button>
             </div>
-            {/* image */}
+            {/* image 1 */}
             <div className='group relative overflow-hidden border-2 border-emerald-400/50 rounded-xl'>
                 {/* overlay */}
                 <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
@@ -33,8 +38,45 @@ const Work = () => {
                   <span className='text-3xl text-white'>Project Title</span>
                 </div>
             </div>
-          </div>
-          <div className='flex-1'>2</div>
+          </motion.div>
+          <motion.div 
+            variants={fadeIn('left', 0.2)}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{ once: false, amount: 0.3 }}
+            className='flex-1 flex flex-col gap-y-10'>
+            {/* image 2 */}
+            <div className='group relative overflow-hidden border-2 border-emerald-400/50 rounded-xl'>
+                {/* overlay */}
+                <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
+                {/* img */}
+                <img className='group-hover:scale-125 transition-all duration-500' src={Img2} alt='' />
+                {/* pretitle */}
+                <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
+                  <span className='text-gradient'>UI/UX Design</span>
+                </div>
+                {/* title */}
+                <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
+                  <span className='text-3xl text-white'>Project Title</span>
+                </div>
+            </div>
+            
+            {/* image 3 */}
+            <div className='group relative overflow-hidden border-2 border-emerald-400/50 rounded-xl'>
+                {/* overlay */}
+                <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
+                {/* img */}
+                <img className='group-hover:scale-125 transition-all duration-500' src={Img3} alt='' />
+                {/* pretitle */}
+                <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
+                  <span className='text-gradient'>UI/UX Design</span>
+                </div>
+                {/* title */}
+                <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
+                  <span className='text-3xl text-white'>Project Title</span>
+                </div>
+            </div>
+          </motion.div>
         </div>
       </div>
     </section>
